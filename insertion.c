fd4c915df1ce31eda3dac84c82f1d5f7c111c608
bhumika-sood <soodbhumika@ymail.com>
2018-08-04 15:04:33 +0530

    updated

--- insertion.c
+++ insertion.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 int i,temp,key,length,a[20];
+void insert();
+void sort();
+void display();
 void main(){
+insert();
+display();
+sort();
+display();
+}
 
 
-
-
+void insert(){
 printf(" no of elements");
 scanf("%d",&length);
 for(i=0;i<length;i++){
 scanf("%d",&a[i]);
-}
+}}
+void sort(){
 for(i=0;i<length;i++){
 key=i;
 while(a[key]>a[key+1] && key>=0 && key<4){
@@ -18,8 +26,9 @@ a[key+1]=a[key];
 a[key]=temp;
 key=key-1;
 }
-}
+}}
 
+void display(){
 printf("the sorted array is");
 for(i=0;i<length;i++){
 printf("%d",a[i]);
